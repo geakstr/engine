453c996c76e2169ec87180f56da8467843026bc1
Dmitry Kharitonov <geakstr@me.com>
2015-01-29 13:12:00 +0300

    Shaders work

--- src/shaders/IShader.java
+++
@@ -1,9 +0,0 @@
-package shaders;
-
-import main.me.geakstr.engine.geometry.VecF;
-import main.me.geakstr.engine.model.Model;
-
-public interface IShader {
-    VecF vertex(Model model, int f_i, int v_i);
-    boolean fragment(VecF bar, int color);
-}
